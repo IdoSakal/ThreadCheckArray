94b4c02955a2b63a4035ecb5eb7ca1e4fc05838d
IdoSakal <idosakal97@gmail.com>
2024-08-16 23:19:09 +0300

    after editing the solution to work with array<int> and added javaDoc where neaded

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,43 +1,97 @@
-public class SharedData 
-{
-	private int [] array;
-	private boolean [] winArray;
-	private boolean flag;
-	private final int b;
-	
-	public SharedData(int[] array, int b) {
-		
-		this.array = array;
-		this.b = b;
-	}
+import java.util.ArrayList;
 
-	public boolean[] getWinArray() 
-	{
-		return winArray;
-	}
+/**
+ * The class representing shared data between threads, support for an integer list, a boolean array,
+ * and a flag. This class provides methods to get and set these values and ensures thread-safety
+ * for the flag property.
+ */
+public class SharedData {
+    
+    /**
+     * The list of integers given from the input.
+     */
+    private ArrayList<Integer> array;
+    // private int [] array;
 
-	public void setWinArray(boolean [] winArray) 
-	{
-		this.winArray = winArray;
-	}
+    
+    /**
+     * An array that hold which location of numbers sum is equal to the required number.
+     */
+    private boolean[] winArray;
+    
+    /**
+     * A flag indicating if thread finished his job finding all the numbers required.
+     */
+    private boolean flag;
+    
+    /**
+     * The required number to find sum in the array.
+     */
+    private final int b;
 
-	public int[] getArray() 
-	{
-		return array;
-	}
+    /**
+     * Constructs a new instance of SharedData with the specified integer list and integer value.
+     *
+     * @param array the list of integers from input
+     * @param b the integer value you need to find sum to
+     */
+    public SharedData(/*int[]*/ ArrayList<Integer> array, int b) {
+        this.array = array;
+        this.b = b;
+    }
 
-	public int getB() 
-	{
-		return b;
-	}
+    /**
+     * Returns the boolean array of the thread that found all the numbers that build the required number first.
+     *
+     * @return the boolean array
+     */
+    public boolean[] getWinArray() {
+        return winArray;
+    }
 
-	public boolean getFlag() 
-	{
-		return flag;
-	}
+    /**
+     * Sets the boolean array of the thread that found all the numbers that build the required number first.
+     *
+     * @param winArray the boolean array to set
+     */
+    public void setWinArray(boolean[] winArray) {
+        this.winArray = winArray;
+    }
 
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
+    /**
+     * Returns the list of integers received to find if some  of their sum equals the required number.
+     *
+     * @return the list of integers
+     */
+    public ArrayList<Integer> getArray() {
+    // public int[] getArray()
+        return array;
+    }
+    
+    /**
+     * Returns the value that the threads required to find number in array that they sum equal to his.
+     *
+     * @return the final integer value
+     */
+    public int getB() {
+        return b;
+    }
 
+    /**
+     * Returns the flag indicating if thread found the numbers to build the desired number.
+     *
+     * @return the flag
+     */
+    public boolean getFlag() {
+        return flag;
+    }
+
+    /**
+     * Sets the flag indicating thread found the numbers to build the desired number.. This method is synchronized to ensure thread-safety.
+     *
+     * @param flag the flag to set
+     */
+    public synchronized void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
